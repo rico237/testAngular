71c1a7912df925d16a3234e454ce8cadf2a5621d
Herrick Wolber <wolbereric@yahoo.fr>
2018-10-05 16:59:42 +0200

    Ajout de la librairie Chart.js

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import { InfodisplayComponent } from './infodisplay/infodisplay.component';
+import { ChartComponent } from '../chart/chart.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HeaderComponent, LayoutComponent, InfodisplayComponent],
+  declarations: [HeaderComponent, LayoutComponent, InfodisplayComponent, ChartComponent],
   exports: [LayoutComponent]
 })
 export class UiModule { }
